8a4e6c922477e383e66b181af07304c081f91176
Yuto Takamoto <ytakamoto@etsy.com>
2019-05-08 00:43:02 -0400

    Update submodules before starting setup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 # NOTE: Make sure to run this from root of repo.
 
+# First make sure we pull down our secrets
+git submodule update
+
 # Setups up the not-really-vendors folder with PHPMailer
 wget https://github.com/PHPMailer/PHPMailer/archive/master.zip
 unzip master.zip
@@ -12,3 +15,4 @@ rm master.zip
 # Makes sure prev_known_ip exists.
 PREV_KNOWN_IP_LOC=$(php -r "require_once('ip_check.php'); echo IP_Checker::PREV_IP_FILE_LOC;")
 touch $PREV_KNOWN_IP_LOC
+
