881a9e3f92a5e06552d0cd28b9384748e7243566
Yuto Takamoto <ytakamoto@etsy.com>
2019-05-08 00:28:41 -0400

    Add $SCRIPT_PATH

--- cronpulls.sh
+++ cronpulls.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-source secrets/config
+
+# This assumes it's running on Ubuntu really.
+# It's nigh impossible to have a 100% platform-independent way of getting script path so I just used one specific to our ubuntu install.
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+source $SCRIPT_PATH/secrets/config
 REPO_DIRS=($(echo $REPO_PATHS_SECRET | tr ':' "\n"))
 
 for REPO_DIR in "${REPO_DIRS[@]}"
