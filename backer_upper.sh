8378149fa83763cb34bbf7f5af172fca0400c6c8
Yuto Takamoto <ytakamoto@etsy.com>
2019-05-08 00:30:59 -0400

    Add $SCRIPT_PATH to backer_upper too

--- backer_upper.sh
+++ backer_upper.sh
@@ -2,7 +2,12 @@
 
 # This script will simply take the newest file in $BACKUP_DIR and move it to $PERMANENT_BACKUP_DIR
 
-source secrets/backer_upper_secrets
+
+# This assumes it's running on Ubuntu really.
+# It's nigh impossible to have a 100% platform-independent way of getting script path so I just used one specific to our ubuntu install.
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+
+source $SCRIPT_PATH/secrets/backer_upper_secrets
 NEWEST_FILE=$(ls -t $BACKUP_DIR | head -1)
 
 cp $BACKUP_DIR/$NEWEST_FILE $PERMANENT_BACKUP_DIR
