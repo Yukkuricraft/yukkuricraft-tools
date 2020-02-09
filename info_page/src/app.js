02f7efb3b8130174d6e467670ff3380616db9c24
Katrix <katrix97@hotmail.com>
2020-02-09 20:14:15 +0100

    Generate staff list dynamically

--- info_page/src/app.js
+++ info_page/src/app.js
@@ -1,3 +1,13 @@
 import 'bootstrap';
 import './scss/app.scss';
 import './index.html';
+
+import $ from 'jquery'
+
+import {createStaffList} from './staffRenderer'
+
+document.addEventListener('DOMContentLoaded', onReady, false);
+
+function onReady() {
+	createStaffList();
+}
