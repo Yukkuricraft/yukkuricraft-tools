548edc795fbdd627ead98b6fcb447a4a0399e7e3
Katrix <katrix97@hotmail.com>
2020-02-12 15:38:06 +0100

    Fix MC profile call, and render with fallback before we have the profile name

--- info_page/src/staffRenderer.js
+++ info_page/src/staffRenderer.js
@@ -2,53 +2,80 @@ import $ from "jquery";
 import {staff} from "./staff";
 
 async function mcUsername(uuid, fallback) {
+	let errorMsg = `Failed to get name for uuid ${uuid}, using fallback ${fallback} instead`;
+
 	try {
-		let res = await fetch('https://api.minetools.eu/profile/' + uuid.replace('-', ''))
+		let res = await fetch('https://api.minetools.eu/profile/' + uuid.replaceAll('-', ''));
 
 		if (res.status !== 200) {
+			console.warn(errorMsg);
 			return fallback;
 		} else {
 			let profile = await res.json();
-			return profile.decodec.profileName
+
+			if(typeof profile.error !== 'undefined') {
+				console.warn(errorMsg + '. Error: ' + profile.error);
+				return fallback;
+			}
+			else {
+				return profile.decoded.profileName
+			}
 		}
 	} catch (e) {
+		console.warn(errorMsg + '. Error: ' + e);
 		return fallback
 	}
 }
 
-async function createAccountsNode(accounts) {
-	async function createSingleNode(account) {
+function createAccountsNode(accounts) {
+	function createSingleNode(account) {
+		let imgNode = $(`<img src="https://mc-heads.net/avatar/${account.uuid}/32">`);
+		let textNode = document.createTextNode(' ' + account.name);
+
+		let setRealUsername = function () {
+			mcUsername(account.uuid, account.name).then(name => {
+				console.log('Getting real name for ' + JSON.stringify(account));
+				textNode.data = ' ' + name;
+				console.log('Set real name for ' + name);
+			})
+		};
+
 		return [
-			$(`<img src="https://mc-heads.net/avatar/${account.uuid}/32">`),
-			document.createTextNode(' ' + await mcUsername(account.uuid, account.name))
+			[
+				imgNode,
+				textNode
+			],
+			setRealUsername
 		]
 	}
 
 	if (accounts.length > 1) {
-		let accountNodes = await Promise.all(accounts.map(createSingleNode));
-		return $('<ul class="list-unstyled">').append(accountNodes.map(nodes => $('<li>').append(nodes)))
+		let [accountNodes, callbacks] = unzip(accounts.map(createSingleNode));
+		return [$('<ul class="list-unstyled">').append(accountNodes.map(nodes => $('<li>').append(nodes))), callbacks]
 	} else if (accounts) {
-		return $('<div>').append(await createSingleNode(accounts[0]))
+		let [node, callback] = createSingleNode(accounts[0]);
+		return [$('<div>').append(node), callback]
 	} else {
 		return $("None")
 	}
 }
 
-async function createMemberNode(hasDescription, member) {
+function createMemberNode(hasDescription, member) {
 	let accountsArr = Array.isArray(member.mcAccounts) ? member.mcAccounts : [member.mcAccounts];
-	let accounts = await createAccountsNode(accountsArr);
+	let [accounts, callbacks] = createAccountsNode(accountsArr);
 
-	let row = $('<tr>');
+	let row = $('<tr>').append($(`<td>${member.name}</td>`), $('<td>').append(accounts));
 
 	if (hasDescription) {
 		let desc = member.description ? member.description : '';
-		return row.append($(`<td>${member.name}</td>`), $('<td>').append(accounts), $('<td>').append(desc));
+		row.append($('<td>').append(desc));
+		return [row, callbacks];
 	} else {
-		return row.append($(`<td>${member.name}</td>`), $('<td>').append(accounts));
+		return [row, callbacks];
 	}
 }
 
-async function makeStaffNode(obj) {
+function makeStaffNode(obj) {
 	let elem = $('<table>');
 	elem.id = 'staff-' + obj.id;
 	elem.addClass('table');
@@ -71,17 +98,32 @@ async function makeStaffNode(obj) {
 	let accountNameCell = $(`<th colspan="${obj.hasDescription ? '2' : '1'}">MC Accounts</th>`);
 	elem.append($('<thead>').append($('<tr>').append(nameCell, accountNameCell)));
 
-	let memberNodes = await Promise.all(obj.members.map(member => createMemberNode(obj.hasDescription, member)));
+	let [memberNodes, callbacks] = unzip(obj.members.map(member => createMemberNode(obj.hasDescription, member)));
 
 	elem.append(memberNodes);
 
-	return elem
+	return [elem, callbacks.flat()]
+}
+
+function unzip(xs) {
+	let res = [[], []];
+
+	for (let i = 0; i < xs.length; i++) {
+		res[0].push(xs[i][0]);
+		res[1].push(xs[i][1]);
+	}
+
+	return res
 }
 
 export function createStaffList() {
 	let staffData = $('#staffData');
 	staffData.innerHTML = '';
-	Promise.all(staff.map(makeStaffNode)).then(staffNodes => {
-		staffData.append(staffNodes)
-	});
+	let [nodes, callbacks] = unzip(staff.map(makeStaffNode));
+
+	staffData.append(nodes);
+
+	for (let fn of callbacks.flat()) {
+		fn();
+	}
 }
