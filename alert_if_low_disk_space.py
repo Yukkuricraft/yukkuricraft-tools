f5e997d11d77099da08ca090b78ab8f572ffc248
remiscarlet <scarletjaeger@gmail.com>
2021-01-13 11:05:49 -0500

    Add alert_if_low_disk_space

---
+++ alert_if_low_disk_space.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import psutil
+import requests
+import subprocess
+from datetime import datetime
+import secrets.discord_secrets as secrets
+
+DEVICES = {
+    "Main Disk": ("/", 85.0), # Dict[str, Tuple[str, float]] - Key is human readable name. Tuple vals are path and disk usage percent to alert on
+    #"Raid Backup Disk": ("/media/raidbackups", 85.0),
+    "Failover Backup Disk": ("/media/backups", 98.0),
+}
+
+for dev_name, data in DEVICES.items():
+    path, threshold_percentage = data
+    percent_used = psutil.disk_usage(path)[3]
+    if percent_used > threshold_percentage:
+        payload = {
+            "content": "Yo (%s), the `%s` is over %.2f%% disk space usage. You should clean it up."
+            % (secrets.REMI_USER_ID, dev_name, threshold_percentage)
+        }
+
+        requests.post(
+            secrets.DISCORD_WEBHOOK_URL,
+            json=payload,
+            headers={"content-type": "application/json"},
+        )
