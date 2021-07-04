2320900094e2492948ca46f7f580b31b61c3c53a
Katrix <katrix97@hotmail.com>
2021-07-04 18:00:13 +0000

    Remove stray newline

--- yc14_server_keepalive.sh
+++ yc14_server_keepalive.sh
@@ -27,7 +27,7 @@ screen -dmS $SCREEN_NAME \
         java -Xms${RAM} -Xmx${RAM} \
         -Duser.dir=${JAR_PATH} \
         -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 \
-        -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \ 
+        -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC \
         -XX:+AlwaysPreTouch -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 \
         -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1HeapWastePercent=5 \
         -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 \
