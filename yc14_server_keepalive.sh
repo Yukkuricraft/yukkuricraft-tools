84a87560a7644aa24d6bbd222f2a9c4753a5eccf
Katrix <katrix97@hotmail.com>
2021-12-26 14:53:05 +0000

    Remove forceUpgrade again

--- yc14_server_keepalive.sh
+++ yc14_server_keepalive.sh
@@ -30,4 +30,4 @@ screen -dmS $SCREEN_NAME \
         -XX:+DisableExplicitGC -XX:TargetSurvivorRatio=90 -XX:+AlwaysPreTouch -XX:+ParallelRefProcEnabled \
         -XX:-UseBiasedLocking -XX:+ExplicitGCInvokesConcurrent \
         -Xss8m \
-        -jar "$JAR_PATH/$JAR_FILE" nogui --forceUpgrade
+        -jar "$JAR_PATH/$JAR_FILE" nogui
